434aa55ee358d0afb6b6feb49080753a6a77ee64
johnfn <johnfn@gmail.com>
2018-11-30 23:36:51 -0800

    bowei2

--- src/game/main.ts
+++ src/game/main.ts
@@ -15,7 +15,7 @@ export class State {
 
   constructor(stage: PIXI.Container) {
     this.stage = stage;
-    this.graph = Graph.generate(600, 100, 10);
+    this.graph = Graph.generate({ width: 600, height: 100, spacing: 10 });
   }
 }
 
