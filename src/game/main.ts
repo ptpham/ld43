003e15208ba9733dbff25103aac1bb75362132c2
Cindy Zhang <cixzhang@gmail.com>
2018-12-01 00:23:58 -0800

    adding to stage

--- src/game/main.ts
+++ src/game/main.ts
@@ -58,6 +58,7 @@ export class Game {
     const stest = new PIXI.Sprite(PIXI.loader.resources['test'].texture);
     stest.x = 10;
     stest.y = 10;
+    this.stage.addChild(stest);
 
     this.animate();
 
