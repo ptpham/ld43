4441e3d2d5579872e84e4fd1d72ceeac0679d2ad
johnfn <johnfn@gmail.com>
2018-11-30 23:54:24 -0800

    bowei2

--- src/game/main.ts
+++ src/game/main.ts
@@ -24,6 +24,7 @@ export class State {
 export class Game {
   stage   !: PIXI.Container;
   state   !: State;
+  render  !: PIXI.WebGLRenderer | PIXI.CanvasRenderer;
 
   constructor(div: HTMLDivElement) {
     this.setUpPixiStuff(div);
