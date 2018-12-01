01cc0d9f6aea00e9772ed10870a9f473c4a1dca6
johnfn <johnfn@gmail.com>
2018-11-30 23:14:08 -0800

    bowei2

---
+++ src/components/card.tsx
@@ -0,0 +1,61 @@
+import { CardType } from "../game/data";
+
+type Card {
+  card: CardType;
+}
+
+export class CardChooser extends React.Component<CardChooserProps, CardChooserState> {
+  public static Instance: CardChooser;
+
+  constructor(props: CardChooserProps) {
+    super(props);
+
+    CardChooser.Instance = this;
+    this.state = {
+      text: "ok",
+      cards: [
+        {
+          type: "Builder",
+          meat: 3,
+          skill: 7,
+        },
+        {
+          type: "Storyteller",
+          meat: 7,
+          skill: 2,
+        },
+        {
+          type: "Fighter",
+          meat: 1,
+          skill: 1,
+        },
+        {
+          type: "Stupid",
+          meat: 1,
+          skill: 9,
+        },
+      ]
+    };
+  }
+
+  public update(state: CardChooserState) {
+    this.setState(state);
+  }
+
+  public render(): JSX.Element {
+    return (
+      <div
+        onClick={ () => console.log('was clicked!') }
+        style={{
+          width: "600px",
+          height: "200px",
+          padding: "20px",
+          border: "1px solid lightgray",
+        } }
+      >
+        Card Chooser 9000
+        { this.state.text }
+      </div>
+    );
+  }
+}
