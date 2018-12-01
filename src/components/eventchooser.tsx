5103e4099a0c7779d5e0518cff490eca1bce9917
johnfn <johnfn@gmail.com>
2018-12-01 15:51:02 -0800

    bowei3

--- src/components/eventchooser.tsx
+++ src/components/eventchooser.tsx
@@ -50,7 +50,7 @@ export class EventChooser extends React.Component<EventChooserProps> {
     }
 
     let targetSkill = data.targetSkill;
-    let hasSkill = _.find([...gameState.cardsInCaravan.keys()], card => card.type === targetSkill) != null;
+    let hasSkill = _.find([...gameState.cardsInCaravan.keys()], card => card.skill === targetSkill) != null;
 
     return <div className="column">
       Woah you are on a {locationType}.
