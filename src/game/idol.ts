70fc4cb3e15617a844543742869711df8652b7a4
johnfn <johnfn@gmail.com>
2018-12-01 14:05:47 -0800

    bowei3

--- src/game/idol.ts
+++ src/game/idol.ts
@@ -14,15 +14,5 @@ export class Idol extends PIXI.Sprite implements IEntity {
   }
 
   update(state: State): void {
-    let start = new Point(this.x, this.y);
-    let { caravan_location } = state;
-
-    if (state.walkAnimation == null && start.distance(caravan_location.position) > 1e-6) {
-      state.walkAnimation = Animations.makeBoundedTicker(120, (delta, t) => {
-        let current = start.lerp(caravan_location.position, t);
-        this.x = current.x;
-        this.y = current.y;
-      }, () => state.walkAnimation = undefined);
-    }
   }
 }
