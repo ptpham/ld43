f382183d297a1b3c47f3cbf58254e7d69a4609db
johnfn <johnfn@gmail.com>
2018-11-30 23:05:35 -0800

    bowei2

--- src/game/lib/point.ts
+++ src/game/lib/point.ts
@@ -20,7 +20,7 @@ export class Point {
     return x instanceof Point;
   }
 
-  constructor(props: { x: number, y: number } = { x: 0, y: 0}) {
+  constructor(props: { x: number, y: number } | number, yOrNothing?: number) {
     this._x = props.x;
     this._y = props.y;
   }
