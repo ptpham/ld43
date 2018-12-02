bedce7f2f7928b0fb5c226d3b7b55d751f26ce82
johnfn <johnfn@gmail.com>
2018-12-01 22:22:51 -0800

    bowei4

--- src/game/blightmanager.ts
+++ src/game/blightmanager.ts
@@ -1,5 +1,12 @@
 
+import { State } from './state';
 
 export class BlightManager {
   // floo
+  public applyBlightAndRenderImminent(state: State, node: Node, idol_time: number): void {
+
+  }
+  public renderImminent(state: State, node: Node, idol_time: number): void {
+
+  }
 }
