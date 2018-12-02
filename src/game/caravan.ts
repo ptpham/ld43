33037bfc5ef58d007538a5db2c0e24753d89dfcc
Cindy Zhang <cixzhang@gmail.com>
2018-12-02 11:06:28 -0800

    idol is carried

--- src/game/caravan.ts
+++ src/game/caravan.ts
@@ -7,15 +7,19 @@ import { Point } from './lib/point';
  * If you want to update the node of the caravan, use state.caravan_location
  */
 export class Caravan extends PIXI.Sprite implements IEntity {
+  idol: PIXI.Sprite;
+
   constructor() {
     super(
       PIXI.loader.resources['caravan'].texture
     );
+    this.idol = new PIXI.Sprite(PIXI.loader.resources['idol'].texture);
+    this.idol.y = -12;
   }
 
   update(state: State): void {
     let start = new Point(this.x, this.y);
-    let { caravanLocation } = state;
+    let { caravanLocation, idolState } = state;
 
     if (state.walkAnimation == null && start.distance(caravanLocation.position) > 1e-6) {
       state.walkAnimation = Animations.makeBoundedTicker(120, (delta, t) => {
@@ -23,6 +27,12 @@ export class Caravan extends PIXI.Sprite implements IEntity {
         this.x = current.x;
         this.y = current.y;
       }, () => state.walkAnimation = undefined);
+    }
+
+    if (idolState.state === "carried") {
+      this.addChildAt(this.idol, 0);
+    } else {
+      this.removeChild(this.idol);
     }
   }
 }
