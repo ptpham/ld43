c3e6f5054118daa2f4e0a5656d3b042e6e642189
johnfn <johnfn@gmail.com>
2018-12-01 20:33:13 -0800

    bowei4

--- src/game/events.tsx
+++ src/game/events.tsx
@@ -1,31 +1,46 @@
 import { LocationType, SkillType } from "./data";
 
 export type EventOutcome = 
-  | { type: "gain-meat"; amount: number }
-  | { type: "lose-meat"; amount: number }
+  | { type: "gain-meat"; amount: number; additionalInfo?: string }
+  | { type: "lose-meat"; amount: number; additionalInfo?: string }
 
 export type EventOption = {
   skillRequired: SkillType;
-  description  : JSX.Element;
+  description  : string;
   outcome      : EventOutcome;
 }
 
 export type EventType = {
   location   : LocationType;
-  description: JSX.Element;
+  description: string;
   difficulty : 1 | 2 | 3;
   options    : EventOption[];
 }
 
 export const AllEvents: EventType[] = [
   {
-    location  : "Forest",
-    difficulty: 1,
-    options   : [
+    location   : "Forest",
+    description: "You come to an eerie, misty forest.",
+    difficulty : 1,
+    options    : [
       {
         skillRequired: "WoodCutter",
-        description: <span>Cut the forest down, one log at a time</span>
-      }
+        description: "Cut the forest down, one log at a time",
+        outcome: { 
+          type: "lose-meat", 
+          amount: 10,
+        },
+      },
+
+      {
+        skillRequired: "Builder",
+        description: "",
+        outcome: { 
+          type: "lose-meat", 
+          amount: 10,
+        },
+      },
+
     ]
   }
 ]
