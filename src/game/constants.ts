e8693e4e83d5256601c5048f5409cc21b164b50f
johnfn <johnfn@gmail.com>
2018-12-01 22:47:29 -0800

    bowei4

--- src/game/constants.ts
+++ src/game/constants.ts
@@ -50,6 +50,26 @@ export const Sample = <T> (x: T[]): T | undefined => {
   return x[Math.floor(SeedRandom() * x.length)];
 }
 
+export const SeedRandomGenerator = (function(manualSeed: number | null = null) {
+  // 0x2F6E2B1
+
+  var seed = manualSeed || (Debug.SeedRandom ? 0x2F6E2B2 : Math.random());
+
+	return function() {
+
+		// Robert Jenkins’ 32 bit integer hash function
+		seed = ((seed + 0x7ED55D16) + (seed << 12))  & 0xFFFFFFFF;
+		seed = ((seed ^ 0xC761C23C) ^ (seed >>> 19)) & 0xFFFFFFFF;
+		seed = ((seed + 0x165667B1) + (seed << 5))   & 0xFFFFFFFF;
+		seed = ((seed + 0xD3A2646C) ^ (seed << 9))   & 0xFFFFFFFF;
+		seed = ((seed + 0xFD7046C5) + (seed << 3))   & 0xFFFFFFFF;
+    seed = ((seed ^ 0xB55A4F09) ^ (seed >>> 16)) & 0xFFFFFFFF;
+
+    const result = (seed & 0xFFFFFFF) / 0x10000000;
+    return result;
+	};
+})
+
 export const SeedRandom = (function(manualSeed: number | null = null) {
   // 0x2F6E2B1
 
