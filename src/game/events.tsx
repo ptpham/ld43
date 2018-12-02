2c67ebc8e01dee8b25837f9cea17446d1f585ce3
johnfn <johnfn@gmail.com>
2018-12-01 21:49:09 -0800

    bowei4

--- src/game/events.tsx
+++ src/game/events.tsx
@@ -5,7 +5,7 @@ export type EventOutcome =
   | { type: "lose-meat"; amount: number; additionalInfo?: string }
 
 export type SkillRequirement =
-  | { type: "specific-skill"; skill: SkillType; withoutSkill: "invisible" | "unlabeled" | "everything" }
+  | { type: "specific-skill"; skill: SkillType; withoutSkill: "Invisible" | "Unlabeled" | "Everything" }
   | { type: "no-skill"      ; }
 
 export type EventOption = {
