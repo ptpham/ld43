f79c8805f1ff86d1234f3c241b3040a19f1a1544
Bowei Liu <liubowei@gmail.com>
2018-12-02 17:07:38 -0800

    fool in golbin nest

--- src/game/events.tsx
+++ src/game/events.tsx
@@ -417,6 +417,18 @@ const GoblinNestFiller: EventType = {
   difficulty   : EventDifficulty.NothingHappens,
   stopsProgress: false,
   options: [
+    {
+      skillRequired: {type: "specific-skill", skill: "Fool", withoutRequirement: "Invisible"},
+      description: "Go into the houses and break all the pots",
+      followUpText: "You let the fool do as he pleases. Unfortunately you find out this is a mistake as he goes into all the houses, breaks all the pots, and molests a few chickens. The chickens are not pleased, and somehow more chickens appear exact revenge upon the party. You try to remember not to molest the chickens next time. ",
+      outcome: [{ type: 'lose-meat', amount: 60, hidden: true}]
+    },
+    {
+      skillRequired: {type: "specific-skill", skill: "Architect", withoutRequirement: "Unlabeled"},
+      description: "Turn some of the dwellings into human-size shelters.",
+      followUpText: "It takes some ingenuity but you are able to convert some of the abandoned goblin dwellings to human-sized ones. They are pretty rickety, though, and probably won't last very long after you leave.",
+      outcome: [{ type: 'lose-meat', amount: 0, hidden: false}]
+    },
     PassOn({ price: 5 }),
   ]
 };
