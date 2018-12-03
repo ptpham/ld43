27358bbe60a1f6ab18dc47d79358cbe39a66f070
Bowei Liu <liubowei@gmail.com>
2018-12-02 16:22:26 -0800

    bard is good in swap

--- src/game/events.tsx
+++ src/game/events.tsx
@@ -446,6 +446,12 @@ const SwampFiller: EventType = {
   difficulty   : EventDifficulty.NothingHappens,
   stopsProgress: false,
   options: [
+    {
+      skillRequired: { type: "specific-skill", skill: "Bard", withoutRequirement: "Everything" },
+      description: "Play some catchy tunes.",
+      followUpText : "When you stop and rest, the bard picks up her instrument and plays you a catchy tune. Suddenly an ogre appears out of the swamp! He says he likes your music, blushing a little. He offers you some swamp flowers and a bag of dried piranha innards. You begrudgingly accept.",
+      outcome: [{ type: "gain-meat", amount: 10, hidden: true, }],
+    },
     PassOn({ price: 10 }),
   ]
 };
