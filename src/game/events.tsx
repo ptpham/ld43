59d80356ba5fc0e508a2cdac7597d72de81d1666
Bowei Liu <liubowei@gmail.com>
2018-12-02 17:53:44 -0800

    goblin nest filler file

--- src/game/events.tsx
+++ src/game/events.tsx
@@ -3,6 +3,7 @@ import { EventDifficulty, EventOption, EventType, PassOn } from './eventDefiniti
 import { DesertEvents } from './eventsByLocation/desert';
 import { MountainEvents } from './eventsByLocation/mountain';
 import { SwampEvents } from './eventsByLocation/swamp';
+import { GoblinNestFillerEvents } from './eventsByLocation/goblinnestfiller';
 
 export const CONTINUE_TEXT = "Continue with your journey.";
 
@@ -354,49 +355,6 @@ const ForestFiller: EventType = {
   ]
 };
 
-const GoblinNestFiller: EventType = {
-  location     : "GoblinNest",
-  description  : "Your party comes to the goblin nest, only to find it abandoned long ago. The mystery nags at you.",
-  difficulty   : EventDifficulty.NothingHappens,
-  stopsProgress: false,
-  options: [
-    {
-      skillRequired: {type: "specific-skill", skill: "Fool", withoutRequirement: "Invisible"},
-      description: "Go into the houses and break all the pots",
-      followUpText: "You let the fool do as he pleases. Unfortunately you find out this is a mistake as he goes into all the houses, breaks all the pots, and molests a few chickens. The chickens are not pleased, and somehow more chickens appear exact revenge upon the party. You try to remember not to molest the chickens next time. ",
-      outcome: [{ type: 'lose-meat', amount: 60, hidden: true}],
-      updateEventTo: {
-        location     : "GoblinNest",
-        description  : "Your party comes to the goblin nest, only to find it abandoned long ago. The mystery nags at you.",
-        difficulty   : EventDifficulty.NothingHappens,
-        stopsProgress: false,
-        options: [
-          {
-            skillRequired: {type: "specific-skill", skill: "Fool", withoutRequirement: "Invisible"},
-            description: "Go into the houses and break all the pots",
-            followUpText: "Somehow, you let the fool run amok in the village, again. This time the chickens are not so merciful.",
-            outcome: [{ type: 'lose-meat', amount: 60, hidden: false}, {type: 'lose-member-weak', skill: 'Fool', hidden: true}],
-          },
-          {
-            skillRequired: {type: "specific-skill", skill: "Architect", withoutRequirement: "Unlabeled"},
-            description: "Turn some of the dwellings into human-size shelters.",
-            followUpText: "It takes some ingenuity but you are able to convert some of the abandoned goblin dwellings to human-sized ones. They are pretty rickety, though, and probably won't last very long after you leave.",
-            outcome: [{ type: 'lose-meat', amount: 0, hidden: false}]
-          },
-          PassOn({ price: 5 }),
-        ]
-      }
-    },
-    {
-      skillRequired: {type: "specific-skill", skill: "Architect", withoutRequirement: "Unlabeled"},
-      description: "Turn some of the dwellings into human-size shelters.",
-      followUpText: "It takes some ingenuity but you are able to convert some of the abandoned goblin dwellings to human-sized ones. They are pretty rickety, though, and probably won't last very long after you leave.",
-      outcome: [{ type: 'lose-meat', amount: 0, hidden: false}]
-    },
-    PassOn({ price: 5 }),
-  ]
-};
-
 const BarbarianVillageFiller: EventType = {
   location     : "BarbarianVillage",
   description  : "Your party comes to the barbarian village - but apparently it's filled with very sleepy barbarians who are more interested in napping than coming out to greet you.",
@@ -455,7 +413,7 @@ export const AllEvents: EventType[] = [
   // GoblinNest
 
   GoblinNest,
-  GoblinNestFiller,
+  ...GoblinNestFillerEvents,
 
   // BarbarianVillage
 
